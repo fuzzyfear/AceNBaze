666221ec9ad55e3dbba0e5f4526119f778b45e02
Pontus Johansson <pontusjohansson8@outlook.com>
2020-05-26 20:51:56 +0200

    Player can now move with the mouse

---
+++ AceNBaze/Assets/Scripts1/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 startPos;
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    void Update()
+    {
+        transform.position = startPos + player.transform.position;
+    }
+}
