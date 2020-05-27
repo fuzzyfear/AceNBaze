c72323e0062ac3421ff423956c542fafbc9b7ed6
Pontus Johansson <pontusjohansson8@outlook.com>
2020-05-27 11:55:12 +0200

    healthbar, targetDummy, characterInfo

---
+++ AceNBaze/Assets/Scripts1/TargetDummyBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TargetDummyBehaviour : MonoBehaviour
+{
+	public Canvas canvas;
+	public Slider hp;
+	public CharacterInfo dummyStats;
+	private Slider characterHp;
+	public Camera cam;
+	public Vector3 camOffset;
+	private void OnEnable()
+	{
+		characterHp = Instantiate(hp, transform.position, Quaternion.identity);
+		characterHp.transform.SetParent(canvas.transform);
+		characterHp.maxValue = dummyStats.HP;
+		characterHp.value = characterHp.maxValue;
+	}
+
+	private void Update()
+	{
+		characterHp.transform.position = cam.WorldToScreenPoint(transform.position + camOffset);
+	}
+
+	public void TakeDmg(int dmg)
+	{
+		characterHp.value -= dmg;
+	}
+}
