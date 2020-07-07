e4deb2825d7d916fd8f68e975fee5dcec55418d0
Franz Jonzon <a17frajo@student.his.se>
2020-07-07 14:34:12 +0200

    Första steget av parry systemet
    har påbörjat pary systemet, 
    har skapat en funktipn som beräknar pary data
    vilket består av en float array som innehåller 8 värdern som går mellan 0 -> 1, dessa representera hur mycket karaktären tittar mot den riktnignen. värdena är för 8 olika riktnignera (framåt, bakåt, höger, vänster, framåt-höger, framåt-vänster, bakåt-höger, bakåt-vänster)

--- AceNBaze/Assets/Scripts1/_Spelare_/Functions/Core/DEBUG_LokingAndMovmenDir.cs
+++ AceNBaze/Assets/Scripts1/_Spelare_/Functions/Core/DEBUG_LokingAndMovmenDir.cs
@@ -16,6 +16,27 @@ public class DEBUG_LokingAndMovmenDir : _FunctionBase
     [SerializeField] private Vector3 LokingDir;
     [SerializeField] private Vector3 MovingDir;
 
+
+   public Vector3 f2;
+   public Vector3 b2;
+   public Vector3 r2;
+   public Vector3 l2;
+   public Vector3 fr2;
+   public Vector3 fl2;
+   public Vector3 br2;
+   public Vector3 bl2;
+
+    public float fm;
+    public float bm;
+    public float rm;
+    public float lm;
+    public float frm;
+    public float flm;
+    public float brm;
+    public float blm;
+
+
+
     public DEBUG_LokingAndMovmenDir() : base() { }
 
     public override void Tick(CharacterBaseAbilitys baseAbilitys, Modifier modifier)
@@ -27,40 +48,72 @@ public class DEBUG_LokingAndMovmenDir : _FunctionBase
 
         Vector3 pos = baseAbilitys.mainTransform.position;
 
-        Debug.DrawRay(pos, LokingDir*5f, Color.blue);
-        Debug.DrawRay(pos, MovingDir*5f, Color.red);
 
 
 
-        Vector3 f = new Vector3(  0, 0,  1 );
-        Vector3 b = new Vector3(  0, 0, -1 );
-        Vector3 r = new Vector3(  1, 0,  0 );
-        Vector3 l = new Vector3( -1, 0,  0 );
-
-        Vector3 fr = new Vector3( 0.5f, 0,  0.5f);
-        Vector3 fl = new Vector3(-0.5f, 0,  0.5f);
-        Vector3 br = new Vector3( 0.5f, 0, -0.5f);
-        Vector3 bl = new Vector3(-0.5f, 0, -0.5f);
+        #region Parer directions
+        Vector3 f = new Vector3(  0, 0,  1 )     ;
+        Vector3 b = new Vector3(  0, 0, -1 )     ;
+        Vector3 r = new Vector3(  1, 0,  0 )     ;
+        Vector3 l = new Vector3( -1, 0,  0 )     ;
+        float grad = 0.7f;
+        Vector3 fr = new Vector3( grad, 0,  grad);
+        Vector3 fl = new Vector3(-grad, 0,  grad);
+        Vector3 br = new Vector3( grad, 0, -grad);
+        Vector3 bl = new Vector3(-grad, 0, -grad);
  
+        Debug.DrawRay(pos, r *2f, Color.green);
+        Debug.DrawRay(pos, l *2f, Color.green);
+        Debug.DrawRay(pos, f *2f, Color.green);
+        Debug.DrawRay(pos, b *2f, Color.green);
+        Debug.DrawRay(pos, br*2f, Color.green);
+        Debug.DrawRay(pos, bl*2f, Color.green);
+        Debug.DrawRay(pos, fl*2f, Color.green);
+        Debug.DrawRay(pos, fr*2f, Color.green);
+        #endregion
 
 
-        Debug.DrawRay(pos, r*4f, Color.green);
-        Debug.DrawRay(pos, l*4f, Color.green);
-        Debug.DrawRay(pos, f*4f, Color.green);
-        Debug.DrawRay(pos, b*4f, Color.green);
-        Debug.DrawRay(pos, br*4f, Color.green);
-        Debug.DrawRay(pos, bl*4f, Color.green);
-        Debug.DrawRay(pos, fl*4f, Color.green);
-        Debug.DrawRay(pos, fr*4f, Color.green);
+        float[] test = modifier.commonFunctionMethods.GetCharacterParyData(baseAbilitys);
+
+        fm = test[0];// f2.magnitude;
+        bm = test[1];//b2.magnitude;
+        rm = test[2];//r2.magnitude;
+        lm = test[3];//l2.magnitude;
+        frm = test[4];//fr2.magnitude;
+        flm = test[5];//fl2.magnitude;
+        brm = test[6];//br2.magnitude;
+        blm = test[7];//bl2.magnitude;
+
+        #region how mutch is agent turned toward parer directions
+        f2  = f  * fm;// Mathf.Clamp01( Vector3.Dot(f, LokingDir ));// new Vector3(  0                           , 0,  Mathf.Clamp01(LokingDir.z)  );
+        b2  = b  * bm;// Mathf.Clamp01( Vector3.Dot(b, LokingDir ));//new Vector3(  0                           , 0, -Mathf.Clamp01(-LokingDir.z) );
+        r2  = r  * rm;// Mathf.Clamp01( Vector3.Dot(r, LokingDir ));//new Vector3(  Mathf.Clamp01(LokingDir.x)  , 0,  0                           );
+        l2  = l  * lm;// Mathf.Clamp01( Vector3.Dot(l, LokingDir ));//new Vector3( -Mathf.Clamp01(- LokingDir.x), 0,  0                           );
+        fr2 = fr * frm;// Mathf.Clamp01( Vector3.Dot(fr, LokingDir));
+        fl2 = fl * flm;// Mathf.Clamp01( Vector3.Dot(fl, LokingDir));
+        br2 = br * brm;// Mathf.Clamp01( Vector3.Dot(br, LokingDir));
+        bl2 = bl * blm;// Mathf.Clamp01( Vector3.Dot(bl, LokingDir));
+
+
+    
+
+        
+
+
+        Debug.DrawRay(pos, r2  * 2f, Color.magenta);
+        Debug.DrawRay(pos, l2  * 2f, Color.magenta);
+        Debug.DrawRay(pos, f2  * 2f, Color.magenta);
+        Debug.DrawRay(pos, b2  * 2f, Color.magenta);
+        Debug.DrawRay(pos, br2 * 2f, Color.magenta);
+        Debug.DrawRay(pos, bl2 * 2f, Color.magenta);
+        Debug.DrawRay(pos, fl2 * 2f, Color.magenta);
+        Debug.DrawRay(pos, fr2 * 2f, Color.magenta);
+        #endregion
 
 
 
-
-
-
-
-
-
+        Debug.DrawRay(pos, LokingDir * 2, Color.blue);
+        Debug.DrawRay(pos, MovingDir * 2, Color.red);
 
 
     }
--- AceNBaze/Assets/Scripts1/_Spelare_/Modifier/CommonFunctions/CommonFunctionMethods.cs
+++ AceNBaze/Assets/Scripts1/_Spelare_/Modifier/CommonFunctions/CommonFunctionMethods.cs
@@ -45,4 +45,64 @@ public class CommonFunctionMethods : MonoBehaviour
 
         return mheading / mdist;
     }
+
+
+
+    /// <summary>
+    /// <para>Returns how mutch pary strenght tha character curently has in diffrent directions </para>
+    /// <para>Totaly 8 directions </para>
+    /// <para>returns a float array(parydata):         </para>
+    /// <para>parydata[0] world space forward          </para>
+    /// <para>parydata[1] world space backlwards       </para>
+    /// <para>parydata[2] world space right            </para>
+    /// <para>parydata[3] world space leaft            </para>
+    /// <para>parydata[4] world space forward-right    </para>
+    /// <para>parydata[5] world space forward-leaft    </para>
+    /// <para>parydata[5] world space backlwards-right </para>
+    /// <para>parydata[5] world space backlwards-leaft </para>
+    /// <para> Each dir range between 0 -> 1, grades how strong the pary is in that dir      </para>
+    /// </summary> 
+    /// <returns>float list with pary data</returns>
+    public float[] GetCharacterParyData(CharacterBaseAbilitys baseAbilitys)
+    {
+        float[] parydata  = new float[8];
+
+        Vector3 LokingDir = GetDirAgentToMouse(baseAbilitys);
+
+
+        #region diraction variables
+        const float forward_x  =  0f, forward_z  = 1f;
+        const float backward_x =  0f, backward_z = -1f;
+        const float right_x    =  1f, right_z    = 0f;
+        const float leaft_x    = -1f, leaft_z    = 0f;
+
+        const float forward_right_x  =  0.7f, forward_right_z = 0.7f;
+        const float forward_leaft_x  = -0.7f, forward_leaft_z = 0.7f;
+        const float backward_right_x =  0.7f, backward_right_z = -0.7f;
+        const float backward_leaft_x = -0.7f, backward_leaft_z = -0.7f;
+        #endregion
+
+        #region calcs dot(dir, lookingdir)
+        parydata[0] = (forward_x        * LokingDir.x) + (forward_z        * LokingDir.z);
+        parydata[1] = (backward_x       * LokingDir.x) + (backward_z       * LokingDir.z);
+        parydata[2] = (right_x          * LokingDir.x) + (right_z          * LokingDir.z);
+        parydata[3] = (leaft_x          * LokingDir.x) + (leaft_z          * LokingDir.z);
+        parydata[4] = (forward_right_x  * LokingDir.x) + (forward_right_z  * LokingDir.z);
+        parydata[5] = (forward_leaft_x  * LokingDir.x) + (forward_leaft_z  * LokingDir.z);
+        parydata[6] = (backward_right_x * LokingDir.x) + (backward_right_z * LokingDir.z);
+        parydata[7] = (backward_leaft_x * LokingDir.x) + (backward_leaft_z * LokingDir.z);
+        #endregion
+
+        #region clamps out negativ valus
+        parydata[0] = Mathf.Clamp01(parydata[0]);
+        parydata[1] = Mathf.Clamp01(parydata[1]);
+        parydata[2] = Mathf.Clamp01(parydata[2]);
+        parydata[3] = Mathf.Clamp01(parydata[3]);
+        parydata[4] = Mathf.Clamp01(parydata[4]);
+        parydata[5] = Mathf.Clamp01(parydata[5]);
+        parydata[6] = Mathf.Clamp01(parydata[6]);
+        parydata[7] = Mathf.Clamp01(parydata[7]);
+        #endregion
+        return parydata;
+    }
 }
